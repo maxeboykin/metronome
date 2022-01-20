69b68696fbbf29aa8bacab46c2c4be1785a433fd
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-20 17:33:04 -0500

    decreaseTempoBtn

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ const startStopBtn = document.querySelector(".start-stop");
 const subtractBeats = document.querySelector(".subtract-beats");
 const addBeats = document.querySelector(".add-beats");
 const measureCount = document.querySelector(".measure-count");
+
+let bpm = 140;
+let beatsPerMeasure = 4;
+
+decreaseTempoBtn.addEventListener("click", () => {
+  if(bpm <= 20) return;
+  bpm--;
+  tempoDisplay.textContent = bpm;
+  tempoSlider.value = bpm;
+})
+
+increaseTempoBtn.addEventListener("click", () => {
+  if(bpm >= 280) return;
+  bpm++;
+  tempoDisplay.textContent = bpm;
+  tempoSlider.value = bpm;
+})
+
+tempoSlider.addEventListener('change', () => {
+  bpm = tempoSlider.value;
+  tempoDisplay.textContent = bpm;
+  tempoSlider.value = bpm;
+})

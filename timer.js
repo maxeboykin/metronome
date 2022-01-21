f7b8e7c86a3b930b7f1d30f77abb6a2f1b8df19d
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-21 17:23:24 -0500

    timerJS

--- timer.js
+++ timer.js
@@ -1,15 +1,27 @@
 function Timer(callback, timeInterval) {
   this.timeInterval = timeInterval;
 
-  this,start = () => {
-
+  this.start = () => {
+    //set expected time
+    this.expected = Date.now() + this.timeInterval;
+    this.timeout = setTimeout(this.round, this.timeInterval);
+    console.log('started!');
   }
-
+  //stop timer
   this.stop = () => {
-
+    clearTimeout(this.timeout);
+    console.log('stopped!');
   }
-
+  //method that takes care of running our callback and adjusting the time interval
   this.round = () => {
-
+    let drift = Date.now() - this.expected;
+    callback();
+    this.expected += this.timeInterval;
+    console.log(drift);
+    this.timeout = setTimeout(this.round, this.timeInterval - drift);
   }
 }
+
+const myTimer = new Timer(() => {
+  console.log('it ran!')},1000);
+myTimer.start();

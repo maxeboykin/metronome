c7bc7a984f7e2c0e8c07c686f8c1564deba955e1
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-21 16:00:01 -0500

    timer.js

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const subtractBeats = document.querySelector(".subtract-beats");
 const addBeats = document.querySelector(".add-beats");
 const measureCount = document.querySelector(".measure-count");
 
+const click1 = new Audio("click1.mp3");
+const click2 = new Audio("click2.mp3");
+
+click1.play();
+
 let bpm = 140;
 let beatsPerMeasure = 4;
 let tempoTextString = tempoText.textContent;
